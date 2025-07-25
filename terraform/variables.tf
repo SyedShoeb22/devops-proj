ff4078d9047e0c02a78e4afcd0f5b0df29945062
Syed Shoeb Abrar <99830705+SyedShoeb22@users.noreply.github.com>
2025-07-25 16:36:09 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "gcp_credentials_file" {}
 variable "project" {}
-variable "region" { default = "us-central1" }
-variable "zone"   { default = "us-central1-a" }
+variable "region"    { default = "us-central1" }
+variable "zone"      { default = "us-central1-a" }
 variable "ssh_public_key_path" { default = "~/.ssh/id_rsa.pub" }
