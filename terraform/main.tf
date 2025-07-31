99f9571c26218d3bc07533d5d9565edc5fd11cb2
Syed Shoeb Abrar <99830705+SyedShoeb22@users.noreply.github.com>
2025-07-31 16:28:50 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,13 +7,19 @@ terraform {
   }
 }
 
+locals {
+  credentials = jsondecode(file(var.gcp_credentials_file))
+}
+
 provider "google" {
   credentials = file(var.gcp_credentials_file)
-  project     = var.project_id
+  project     = local.credentials.project_id
   region      = var.region
 }
 
+# ----------------------
 # VM Creation
+# ----------------------
 resource "google_compute_instance" "default" {
   name         = var.vm_name
   machine_type = "n2-standard-8"
@@ -31,72 +37,94 @@ resource "google_compute_instance" "default" {
     access_config {}
   }
 
-  labels = {
-    managed_by = "scheduler"
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.ssh_public_key)}"
   }
 
   tags = ["ssh"]
 }
 
-# Storage bucket for Cloud Functions code
+output "instance_ip" {
+  value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
+}
+
+# ----------------------
+# Storage for Cloud Functions
+# ----------------------
 resource "google_storage_bucket" "functions" {
-  name     = "${var.project_id}-functions-bucket"
+  name     = "${local.credentials.project_id}-functions-bucket"
   location = var.region
 }
 
-# Package Cloud Function code
-resource "null_resource" "package_function" {
-  provisioner "local-exec" {
-    command = <<EOT
-      cd functions && zip -r ../function-code.zip .
-    EOT
-  }
-}
-
+# Upload zipped Cloud Function code
 resource "google_storage_bucket_object" "function_code" {
   name   = "function-code.zip"
   bucket = google_storage_bucket.functions.name
-  source = "function-code.zip"
+  source = "${path.module}/functions/function-code.zip"
 }
 
-# Cloud Functions: Start VM
+# ----------------------
+# Cloud Functions
+# ----------------------
 resource "google_cloudfunctions_function" "start_vm" {
   name        = "start-vm"
   runtime     = "python311"
   entry_point = "start_vm"
+
   source_archive_bucket = google_storage_bucket.functions.name
   source_archive_object = google_storage_bucket_object.function_code.name
-  trigger_http = true
+
+  trigger_http        = true
   available_memory_mb = 128
+  environment_variables = {
+    PROJECT  = local.credentials.project_id
+    ZONE     = var.zone
+    INSTANCE = var.vm_name
+  }
 }
 
-# Cloud Functions: Stop VM
 resource "google_cloudfunctions_function" "stop_vm" {
   name        = "stop-vm"
   runtime     = "python311"
   entry_point = "stop_vm"
+
   source_archive_bucket = google_storage_bucket.functions.name
   source_archive_object = google_storage_bucket_object.function_code.name
-  trigger_http = true
+
+  trigger_http        = true
   available_memory_mb = 128
+  environment_variables = {
+    PROJECT  = local.credentials.project_id
+    ZONE     = var.zone
+    INSTANCE = var.vm_name
+  }
 }
 
-# Cloud Functions: Delete VM
 resource "google_cloudfunctions_function" "delete_vm" {
   name        = "delete-vm"
   runtime     = "python311"
   entry_point = "delete_vm"
+
   source_archive_bucket = google_storage_bucket.functions.name
   source_archive_object = google_storage_bucket_object.function_code.name
-  trigger_http = true
+
+  trigger_http        = true
   available_memory_mb = 128
+  environment_variables = {
+    PROJECT  = local.credentials.project_id
+    ZONE     = var.zone
+    INSTANCE = var.vm_name
+  }
 }
 
+# ----------------------
 # Cloud Scheduler Jobs
+# ----------------------
 resource "google_cloud_scheduler_job" "start_9am" {
-  name        = "start-9am"
-  schedule    = "30 3 * * *" # 9:00 AM IST
-  time_zone   = "Asia/Kolkata"
+  name      = "start-9am"
+  schedule  = "0 3 * * *" # 9:00 AM IST
+  time_zone = "Asia/Kolkata"
+
   http_target {
     http_method = "POST"
     uri         = google_cloudfunctions_function.start_vm.https_trigger_url
@@ -104,9 +132,10 @@ resource "google_cloud_scheduler_job" "start_9am" {
 }
 
 resource "google_cloud_scheduler_job" "stop_11am" {
-  name        = "stop-11am"
-  schedule    = "30 5 * * *" # 11:00 AM IST
-  time_zone   = "Asia/Kolkata"
+  name      = "stop-11am"
+  schedule  = "30 5 * * *" # 11:00 AM IST
+  time_zone = "Asia/Kolkata"
+
   http_target {
     http_method = "POST"
     uri         = google_cloudfunctions_function.stop_vm.https_trigger_url
@@ -114,9 +143,10 @@ resource "google_cloud_scheduler_job" "stop_11am" {
 }
 
 resource "google_cloud_scheduler_job" "start_11_30" {
-  name        = "start-11-30"
-  schedule    = "0 6 * * *" # 11:30 AM IST
-  time_zone   = "Asia/Kolkata"
+  name      = "start-11-30"
+  schedule  = "0 6 * * *" # 11:30 AM IST
+  time_zone = "Asia/Kolkata"
+
   http_target {
     http_method = "POST"
     uri         = google_cloudfunctions_function.start_vm.https_trigger_url
@@ -124,9 +154,10 @@ resource "google_cloud_scheduler_job" "start_11_30" {
 }
 
 resource "google_cloud_scheduler_job" "stop_2pm" {
-  name        = "stop-2pm"
-  schedule    = "30 8 * * *" # 2:00 PM IST
-  time_zone   = "Asia/Kolkata"
+  name      = "stop-2pm"
+  schedule  = "30 8 * * *" # 2:00 PM IST
+  time_zone = "Asia/Kolkata"
+
   http_target {
     http_method = "POST"
     uri         = google_cloudfunctions_function.stop_vm.https_trigger_url
@@ -134,9 +165,10 @@ resource "google_cloud_scheduler_job" "stop_2pm" {
 }
 
 resource "google_cloud_scheduler_job" "start_3pm" {
-  name        = "start-3pm"
-  schedule    = "30 9 * * *" # 3:00 PM IST
-  time_zone   = "Asia/Kolkata"
+  name      = "start-3pm"
+  schedule  = "30 9 * * *" # 3:00 PM IST
+  time_zone = "Asia/Kolkata"
+
   http_target {
     http_method = "POST"
     uri         = google_cloudfunctions_function.start_vm.https_trigger_url
@@ -144,9 +176,10 @@ resource "google_cloud_scheduler_job" "start_3pm" {
 }
 
 resource "google_cloud_scheduler_job" "delete_5pm" {
-  name        = "delete-5pm"
-  schedule    = "30 11 * * *" # 5:00 PM IST
-  time_zone   = "Asia/Kolkata"
+  name      = "delete-5pm"
+  schedule  = "30 11 * * *" # 5:00 PM IST
+  time_zone = "Asia/Kolkata"
+
   http_target {
     http_method = "POST"
     uri         = google_cloudfunctions_function.delete_vm.https_trigger_url
